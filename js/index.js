509cbae9a18994c373b9f56dafa8eddf8ebae31e
Ozmatron <oisin.flanagan@ucdconnect.ie>
2021-05-15 15:50:50 +0100

    index.html template setup

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 function switchTab(name) {
+inActiveTabs = document.getElementsByClassName("tabContent");
+  for (i = 0; i < inActiveTabs.length; i++) {
+    inActiveTabs[i].style.display = "none";
+  }
   var tab = document.getElementById(name);
   if (tab.style.display === "none") {
     tab.style.display = "block";
