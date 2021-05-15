f33cc8c297f190d2243e3ba3a3e45680bfb380c8
Ozmatron <oisin.flanagan@ucdconnect.ie>
2021-05-15 18:08:14 +0100

    styling added, active tab functionality added, tests updated

--- js/index.js
+++ js/index.js
@@ -1,12 +1,27 @@
-function switchTab(name) {
-inActiveTabs = document.getElementsByClassName("tabContent");
+function setActiveTab(tabId, buttonId) {
+  // Set Active Tab onload
+  var tab = document.getElementById(tabId);
+  tab.style.display = "block";
+  document.getElementById(buttonId).className += " active";
+}
+
+function switchTab(tabId, buttonId) {
+  // Reset all tabs
+  inActiveTabs = document.getElementsByClassName("tabContent");
   for (i = 0; i < inActiveTabs.length; i++) {
     inActiveTabs[i].style.display = "none";
   }
-  var tab = document.getElementById(name);
+  inActiveBtns = document.getElementsByClassName("active");
+  for (i = 0; i < inActiveBtns.length; i++) {
+      console.log(inActiveBtns[i])
+      inActiveBtns[i].className = "tabBtns";
+  }
+  // Set switched tab to active
+  var tab = document.getElementById(tabId);
   if (tab.style.display === "none") {
     tab.style.display = "block";
   } else {
     tab.style.display = "none";
   }
+  document.getElementById(buttonId).className = "active";
 }
--- test/test.js
+++ test/test.js
@@ -1,35 +1,33 @@
 describe('switchTab()', function(){
         beforeEach(function(){
-        document.querySelector('body').innerHTML += '<button id="btn" onclick="switchTab("tab1")">Click Me</button><div id="tab1">This is my DIV element.</div>';
+        document.querySelector('body').innerHTML += '<button class="tabBtns" id="btn1" onclick="switchTab("tab1", "btn1")">Tab 1</button><div id="tab1" class="tabContent"><h3>Tab 1</h3><p>Text</p></div>';
     });
-    it('should make tab1 not display', function(){
+    it('should make tab1 display', function(){
 
-        switchTab('tab1');
+        switchTab('tab1','btn1');
 
-        const greeting = document.querySelector('#tab1');
         assert.equal(
             document.getElementById('tab1').style.display, 
-            'none'
+            'block'
         );
         
     });
 });
 
-describe('switchBackToTab()', function(){
+describe('switchBackToAnotherTab()', function(){
         beforeEach(function(){
-        document.querySelector('body').innerHTML += '<button id="btn" onclick="switchTab("tab1")">Click Me</button><div id="tab1">This is my DIV element.</div>';
+        document.querySelector('body').innerHTML += '<button class="tabBtns" id="btn1" onclick="switchTab("tab1", "btn1")">Tab 1</button><div id="tab1" class="tabContent"><h3>Tab 1</h3><p>Text</p></div><button class="tabBtns" id="btn2" onclick="switchTab("tab2", "btn2")">Tab 2</button><div id="tab2" class="tabContent"><h3>Tab 2</h3><p>Text</p> </div>';
     });
         afterEach(function(){
-        switchTab('tab1')
+        switchTab('tab2','btn2')
     });
-    it('should make tab1 display again', function(){
+    it('should make tab2 hide', function(){
         
-        switchTab('tab1')
+        switchTab('tab1','btn1')
     
-        const greeting2 = document.querySelector('#tab1');
         assert.equal(
-            document.getElementById('tab1').style.display, 
-            'block'
+            document.getElementById('tab2').style.display, 
+            'none'
         );
     });
 });
